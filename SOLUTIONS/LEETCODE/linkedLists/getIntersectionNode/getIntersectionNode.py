984395a1a39a0ef3af5d478b030b8012b9278556
Hamilton <dehamiltonglobal@gmail.com>
2025-07-04 12:22:34 +0100

    newSystem

--- SOLUTIONS/LEETCODE/linkedLists/getIntersectionNode/getIntersectionNode.py
+++ SOLUTIONS/LEETCODE/linkedLists/getIntersectionNode/getIntersectionNode.py
@@ -1,14 +1,14 @@
 def getIntersectionNode (headA, headB):
+  # check if either list is empty
   if not headA or not headB : return None
   
   pointerA = headA
   pointerB = headB
   
+  # traverse both lists with dual pointers
   while pointerA is not pointerB:
+    # switch to the next node or the other list's head
     pointerA = pointerA.next if pointerA else headB
     pointerB = pointerB.next if pointerB else headA
     
   return pointerA
-
-
-    
