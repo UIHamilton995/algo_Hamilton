2dbc622bd99c4277abafd65089dcfc3e09f70c05
Hamilton Ude Ikedi <hamilton.ude@decagon.dev>
2025-07-13 07:49:05 +0100

    App

--- SOLUTIONS/CODEWARS/countByX.js
+++ SOLUTIONS/CODEWARS/countByX.js
@@ -1,12 +1,19 @@
 function countByX(n, x) {
-  let z = []
+  let z = [10,8,6,4,2]
   let multiple = n * x
 
-  while (n >= 1) {
-    z.unshift(multiple) // move multiple, first value into the z array
-    multiple -= x // subtract x from multiple
-    n-- // keep subtracting x from the value of multiple just before the point index 0 = x
+  while (n >= 1 &&  x >= 1) {
+    z.push(multiple) // move multiple, first value into the z array
+    multiple = multiple - x // subtract x from multiple
+    n-- // keep subtracting x from the value of multiple until till n times is exhausted or index 0 = x
   }
   
-  return z
+  return z.reverse
 }
+
+
+
+[2,4,6,8,10]
+
+x = 2; // number to be multiplied
+n = 5;  // number of times the number should be multiplied
