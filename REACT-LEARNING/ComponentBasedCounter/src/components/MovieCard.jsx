280db44aa104303fe9d00a7bda404c074905a6cc
Hamilton Ude Ikedi <hamilton.ude@decagon.dev>
2025-08-16 10:14:13 +0100

    perfectedMovieflix

--- REACT-LEARNING/ComponentBasedCounter/src/components/MovieCard.jsx
+++ REACT-LEARNING/ComponentBasedCounter/src/components/MovieCard.jsx
@@ -1,6 +1,5 @@
 import "../css/MovieCard.css"
 import { useMovieContext } from "../contexts/MovieContexts"
-import Favorites from '../pages/Favorites';
 
 const MovieCard = ({ movie }) => {
   const { isFavorites, addToFavorites, removeFromFavorites } = useMovieContext()
@@ -13,22 +12,20 @@ const MovieCard = ({ movie }) => {
   }
   
   return (
-    <>
       <div className="movie-card">
         <div className="movie-poster">
           <img src={`http://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
           <div className="movie-overlay">
-            <button className={`favorite-btn ${ favorite? "active" : ""}`} onClick={onFavouriteClick}>
-              🤍
+            <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavouriteClick}>
+              ♥
             </button>
           </div>
         </div>
         <div className="movie-info">
           <h3>{movie.title}</h3>
-          <p>{movie.release_date?.split(["-"][0])}</p>
+          <p>{movie.release_date}</p>
         </div>
       </div>
-    </>
   )
 }
 
